aa597cffe1128270775fde43f5043e683f73df16
Vojasvi Reddy K <vojasvireddy13k@gmail.com>
2026-02-26 22:39:52 +0530

    Time: 3 ms (32.21%) | Memory: 12.4 MB (95.74%) - LeetSync

---
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        
+        if not strs:
+            return ""
+        
+        first_word = strs[0]
+        
+        for i in range(len(first_word)):
+            
+            for word in strs:
+                
+                if i >= len(word) or word[i] != first_word[i]:
+                    return first_word[:i]
+        
+        return first_word
