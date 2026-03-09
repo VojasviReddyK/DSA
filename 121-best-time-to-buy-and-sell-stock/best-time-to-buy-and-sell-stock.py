3dce2e546cee31152d6ec0df869302006d1f6644
Vojasvi Reddy K <vojasvireddy13k@gmail.com>
2026-03-09 12:26:54 +0530

    Time: 55 ms (72.88%) | Memory: 19.6 MB (13.36%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -1,21 +1,19 @@
 class Solution():
-    def maxProfit(self, nums):
+    def maxProfit(Self, nums):
 
-        min_val = nums[0]
+        min_value = nums[0]
         max_difference = 0
         for i in range(1, len(nums)):
 
-                diff = nums[i] - min_val
+            diff = nums[i] - min_value
+
+            if diff > max_difference:
+                max_difference = diff
+
+            if nums[i] < min_value:
+                min_value = nums[i]
                 
-                if diff > max_difference:
-                    max_difference = diff
-
-                if nums[i] < min_val:
-                    min_val = nums[i]
-
         return max_difference
 
-
-obj = Solution()
-print(obj.maxProfit([7, 1, 5, 3, 6, 4]))
-        
+obj=Solution()
+print(obj.maxProfit([7,1,2,3,6,5]))
