5086a61e10f3199db6ca7b1e58d3f58d16818239
Vojasvi Reddy K <vojasvireddy13k@gmail.com>
2026-03-10 06:51:18 +0530

    Time: 15 ms (43.71%) | Memory: 14.2 MB (87.99%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.py
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.py
@@ -1,3 +1,4 @@
+# BROTE FORCE METHOD O(n log n)
 class Solution(object):
     def sortedSquares(self, nums):
 
@@ -8,4 +9,29 @@ class Solution(object):
         return nums
 obj=Solution()
 print(obj.sortedSquares([-4,-1,0,3,10]))
+
+# TWO POINTERS METHOD
+class Solution():
+    def sortedSquares(self, nums):
+        n = len(nums)
+        result = [0] * n
+        left = 0
+        right = n - 1
+        pos = n - 1
+        while left <= right:
+            left_square = nums[left] ** 2
+            right_square = nums[right] ** 2
+
+            if left_square > right_square:
+                result[pos] = left_square
+                left += 1
+            else:
+                result[pos] = right_square
+                right -=1 
+            pos -= 1
+
+        return result
+obj=Solution()
+print(obj.sortedSquares([-4,0,-1,3,10]))
+
         
