b895e7f9e880d5c4f92198bc5059d8987ce48fea
Vojasvi Reddy K <vojasvireddy13k@gmail.com>
2026-03-10 10:19:19 +0530

    Time: 4 ms (53.44%) | Memory: 12.5 MB (42.14%) - LeetSync

--- 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.py
+++ 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.py
@@ -1,10 +1,26 @@
+# HASHMAP METHOD
 class Solution(object):
-    def checkIfExist(self, arr):
-        for i in range(len(arr)):
-            for j in range(len(arr)):
-                if i != j and arr[i] == 2 * arr[j]:
-                    return True
+    def checkIfExist(self, nums):
+        seen = set()
+        for i in nums:
+            if (2 * i in seen) or (i % 2 == 0 and i // 2 in seen):
+                return True
+            seen.add(i)
         return False
+
 obj=Solution()
 print(obj.checkIfExist([10,2,5,3]))
+
+# # BRUTE FORCE METHOD
+# class Solution(object):
+#     def checkIfExist(self, arr):
+#         for i in range(len(arr)):
+#             for j in range(len(arr)):
+#                 if i != j and arr[i] == 2 * arr[j]:
+#                     return True
+#         return False
+# obj=Solution()
+# print(obj.checkIfExist([10,2,5,3]))
+
+
         
